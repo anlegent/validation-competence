b9d4061a9f360f951fdab8f28a92a69b2427dfc7
anlegent <alegent@edenschool.fr>
2024-03-13 11:48:09 +0100

    added an input to add skills

--- src/App.jsx
+++ src/App.jsx
@@ -4,31 +4,40 @@ import './App.css'
 
 
 function App() {
-  let state = useState(0);
-  let counter = state[0];
-  let changeCounter = state[1];
+const [counter,changeCounter] = useState(0);
+const [countDown,changeCountDown] = useState(100);
+const [inputValue,changeInputValue] = useState("");
 
-  //console.log(counter);
+const onChangeCounter = () => {
+console.log("click");
+changeCounter((previousValue)=> {
+return ++previousValue;
+});
+};
 
-  const log = () => {
-  console.log("click");
-  changeCounter((previousValue)=>{
-  console.log("previousValue: ", previousValue);
-  return previousValue + 1;
-  });
-  }
+const onChangeInputValue = (event) => {
+changeInputValue(() => {
+return event.target.value;
+});
+};
+
+const add = () => {
+console.log(inputValue);
+};
 
   return ( 
   <>
   <h1>coucou!</h1> 
   <p>bienvenue</p>
+  <input value={inputValue} type="text" name="skill" onChange={onChangeInputValue}/>
+  <button onClick={add}>AJOUTER UNE COMPETENCE (apparait dans la console)</button>
   <ul>
   <li>react</li>
   <li>javascript</li>
   <li>css</li>
   </ul>
   <p>nombre de compétences validées : <span >{counter}</span></p>
-  <button onClick={log}>VALIDER UNE COMPETENCE</button>
+  <button onClick={onChangeCounter}>VALIDER UNE COMPETENCE</button>
   </>
   )
 }
